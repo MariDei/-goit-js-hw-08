9a22a31d88f42fd610bb1179a953db72a923e4f9
Marianna Dei <d.marianna@icloud.com>
2024-03-12 23:26:12 +0200

    Updated all files (index.html, styles.css, gallery.js).

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', event => {
-  const name = event.currentTarget.value.trim();
-  nameOutput.textContent = name ? name : 'Anonymous';
-});
